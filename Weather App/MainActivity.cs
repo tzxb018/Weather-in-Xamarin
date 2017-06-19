7f1b3387445e6dda0bc0872fef8d388c0e131930
Tomo Bessho <tzxbApp018@gmail.com>
2017-06-19 12:40:34 -0500

    Init

---
+++ Weather App/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Weather_App
+{
+    [Activity(Label = "Weather_App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
